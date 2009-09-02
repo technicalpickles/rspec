fb1cf147867da8ced8629cf3b58137701e4816f7
Joshua Nichols <josh@technicalpickles.com>
2009-09-02 12:17:42 -0400

    Implemented test spies.
    This allows you to establish a stub, then later verify it was received.
    
        object = Object.new
        object.stub!(:message)
        object.message
        object.should have_received(:message)   # passes
        object.should_not have_receive(:aieeee) # passes
        object.should have_received(:zomg)      # fails

---
+++ spec/spec/matchers/have_received_spec.rb
@@ -0,0 +1,32 @@
+require 'spec/spec_helper'
+module Spec
+  module Matchers
+    describe "[object.should] have_received(method, *args)" do
+      before do
+        @object = String.new
+      end
+
+      it "does match if method is called with correct args" do
+        @object.stub!(:slice)
+        @object.slice(5)
+
+        have_received(:slice, 5).matches?(@object).should be_true
+      end
+
+      it "does not match if method is called with incorrect args" do
+        @object.stub!(:slice)
+        @object.slice(3)
+
+        have_received(:slice, 5).matches?(@object).should be_false
+      end
+
+      it "does not match if method is not called" do
+        @object.stub!(:slice)
+
+        have_received(:slice, 5).matches?(@object).should be_false
+      end
+
+    end
+
+  end
+end
